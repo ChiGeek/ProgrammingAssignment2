fdcca82f4bf9d35308434ffa2441bb8105ad9d10
ChiGeek <awu1@alumni.nd.edu>
2016-09-04 17:32:23 -0500

    Update ProgrammingHW2 for Submission
    makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Creates a special object to store a matrix and caches its inverse
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+##This function creates a matrix and caches its value
+makeCacheMatrix <- function(my_matrix = numeric())
+{
+  global_inverse = NULL
+  set_data <- function(set_matrix)
+  {
+    my_matrix <<- set_matrix
+    globlal_inverse = NULL ## since we replace the input data, we must recompute inverse
+  }
+  get_data <- function() my_matrix
+  set_inverse <- function(inverse) global_inverse <<- inverse
+  get_inverse <- function() global_inverse
+  list(set_data = set_data, get_data = get_data, set_inverse = set_inverse,
+       get_inverse = get_inverse)
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse and caches the value
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) 
+  {
         ## Return a matrix that is the inverse of 'x'
+  
+    m <- x$get_inverse()
+    if(!is.null(m))
+    {
+      message("getting cached data")
+      return(m)
+    }
+  data <- x$get_data()
+  m <- solve(data, ...)
+  x$set_inverse(m)
+  
 }
+
+my_mat <- matrix(c(1,0,5,2,1,6,3,4,0),3,3)
+x <- makeCacheMatrix(my_mat)
+cacheSolve(x)
+print (x$get_inverse())
